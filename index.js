d83fb35085955bcc908ac0249bb0a5af68847859
Gema <tokiobeast1@gmail.com>
2022-10-24 20:29:32 +0700

    added change in index.js

--- index.js
+++ index.js
@@ -2,24 +2,13 @@ const express = require('express');
 const app = express();
 const port = 9090;
 const axios = require('axios');
+const { activityRouter } = require('./router');
 
 app.get(`/`, (req, res) => {
   res.send('Welcome to Bored Api Project');
 });
 
-app.get('/activity', (req, res) => {
-  axios({
-    url: 'http://www.boredapi.com/api/activity/',
-    method: 'get'
-  })
-  .then(response => {
-    res.send(response.data.activity)
-    console.log(response.data);
-  })
-  .catch((err) => {
-    console.log(err)
-  });
-});
+app.use('/activity', activityRouter);
 
 app.listen(port, () => {
   console.log(`Bored Running on ${port}`);
